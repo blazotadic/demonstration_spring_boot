1f7cff8b4fb32c166ef27f22d26e8e90c5b3807d
Blazo <tadic108@gmail.com>
2022-03-17 15:24:21 +0100

    liquibase

--- src/main/java/com/example/demonstration_spring_boot/repository/RoleRepository.java
+++ src/main/java/com/example/demonstration_spring_boot/repository/RoleRepository.java
@@ -4,7 +4,6 @@ import com.example.demonstration_spring_boot.dto.RoleDTO;
 import com.example.demonstration_spring_boot.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
---
+++ src/main/resources/liquibase/seeds/add_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO user(first_name, last_name, username, password, is_active)
+VALUES
+('Petar', 'Petrovic', 'PP@gmail.com', '12345678', true),
+('Stefan', 'Stefanovic', 'SS@gmail.com', '12345678', true),
+('Jovan', 'Jovanovic', 'JJ@gmail.com', '12345678', true),
+('Lazar', 'Lazarevic', 'LL@gmail.com', '12345678', true);
