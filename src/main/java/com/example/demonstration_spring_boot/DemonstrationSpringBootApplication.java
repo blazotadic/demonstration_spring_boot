87ce39ac255dcba2549828054cba5d742b34c358
blazotadic <95979502+blazotadic@users.noreply.github.com>
2022-03-20 19:30:25 +0100

    Update DemonstrationSpringBootApplication.java

--- src/main/java/com/example/demonstration_spring_boot/DemonstrationSpringBootApplication.java
+++ src/main/java/com/example/demonstration_spring_boot/DemonstrationSpringBootApplication.java
@@ -2,8 +2,9 @@ package com.example.demonstration_spring_boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-
+@EnableCaching
 @SpringBootApplication
 public class DemonstrationSpringBootApplication {
 
